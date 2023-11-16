03dd530c46143723e8a99e614586706ba06d3f9a
lifeisbeautiful <poltoradnevone@gmail.com>
2023-11-16 08:54:29 +0400

    feat: final version

--- bot/src/main/java/ru/tinkoff/tictactoebottinkoffsummerpractice/bot/controller/BotController.java
+++ bot/src/main/java/ru/tinkoff/tictactoebottinkoffsummerpractice/bot/controller/BotController.java
@@ -1,7 +1,6 @@
 package ru.tinkoff.tictactoebottinkoffsummerpractice.bot.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ public class BotController {
     private final BotService botService;
 
     @PostMapping("/bot/turn")
-	@CrossOrigin(origins = "http://localhost:5173")
     public GameFieldResponseDto makeTurn(@RequestBody GameFieldRequestDto gameFieldRequestDto) {
         String gameField = gameFieldRequestDto.gameField();
         String newGameField = botService.makeTurnByGameField(gameField);
---
+++ reg.sh
@@ -0,0 +1,3 @@
+curl --location --request PUT 'http://localhost:8080/commands/test-bot-2' \
+--header 'Content-Type: text/plain' \
+--data 'test-bot-2'
